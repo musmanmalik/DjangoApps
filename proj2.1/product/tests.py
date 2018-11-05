b0a6466c2685a25507e54996d726e2966ab01211
musman <muhammadu590@gmail.com>
2018-11-05 17:49:03 +0500

    second2

--- proj2.1/product/tests.py
+++ proj2.1/product/tests.py
@@ -1,15 +1,15 @@
 from django.test import TestCase, Client
 from django.contrib.auth.models import User
-from django.urls import reverse_lazy
+from django.urls import reverse_lazy, reverse
+
 
 
 def setUp(self):
     self.client = Client()
 
 
-
 def test_registration(self):
-    url = reverse_lazy('signup')
+    url = reverse('signup')
 
     # test req method GET
     response = self.client.get(url)
@@ -17,13 +17,18 @@ def test_registration(self):
 
 
 def test_blank_data(self):
-    form = RegisterForm({}, entry=self.entry)
-    self.assertFalse(form.is_valid())
-    self.assertEqual(form.errors, {
-        'username': ['required'],
-        'email': ['required'],
-        'password': ['required'],
-    })
+    url = reverse('signup')
+    response = self.client.post(url, {})
+    self.assertEqual(response.status, 200)
+    exp_data = {
+        'error': True,
+        'errors': {
+            'username': 'This field is required',
+            'email': 'This field is required',
+            'password': 'This field is required',
+        }
+    }
+    self.asssertEqual(exp_data, response.json())
 
 
 def test_get_authors(self):
