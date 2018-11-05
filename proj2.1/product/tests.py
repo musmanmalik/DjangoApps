3d56ac202d7afcf55d4fe15e7ee97c8194ae4707
musman <muhammadu590@gmail.com>
2018-11-05 16:57:03 +0500

    secnd

--- proj2.1/product/tests.py
+++ proj2.1/product/tests.py
@@ -1,2 +1,14 @@
-from django.test import TestCase
+from django.test import TestCase, Client
+from django.contrib.auth.models import User
+from django.urls import reverse_lazy
 
+def setUp(self):
+    self.client = Client()
+
+
+def test_registration(self):
+    url = reverse_lazy('signup')
+
+    # test req method GET
+    response = self.client.get(url)
+    self.assertEqual(response.status, 200)
